970d99431767ff4a8f964d4c0b59b56958f90401
lidonglifighting <42424903+lidonglifighting@users.noreply.github.com>
2021-12-23 16:58:51 +0800

    add dbmaker in appveyor

---
+++ appveyor/installdbmaker.cmd
@@ -0,0 +1,50 @@
+set DBMAKERPATH=%cd%
+echo %DBMAKERPATH%
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`%PYTHON_HOME%\python -c "import sys; sys.stdout.write(str(sys.version_info.major))"`) DO (
+SET PYTHON_MAJOR_VERSION=%%F
+)
+FOR /F "tokens=* USEBACKQ" %%F IN (`%PYTHON_HOME%\python -c "import sys; sys.stdout.write('64' if sys.maxsize > 2**32 else '32')"`) DO (
+SET PYTHON_ARCH=%%F
+)
+
+IF %PYTHON_ARCH% EQU 32 (
+  echo "install 32bit dbmaker"
+  powershell curl -o dbmaker.zip https://www.dbmaker.com/download/5.4.4Beta/dbmaker-5.4.4Bundle-WIN32.zip
+) ELSE (
+  echo "install 64bit dbmaker"
+  powershell curl -o dbmaker.zip https://www.dbmaker.com/download/5.4.4Beta/dbmaker-5.4.4Bundle-WIN64.zip
+)
+powershell -command "Expand-Archive -Force 'dbmaker.zip' %DBMAKERPATH%"
+
+echo "Installing the driver..."
+IF %PYTHON_ARCH% EQU 32 (
+echo "32bit odbc driver"
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v Driver /t REG_EXPAND_SZ /d %DBMAKERPATH%\bundle\DMAPI54.DLL /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v Setup /t REG_EXPAND_SZ /d %DBMAKERPATH%\bundle\DMSET.DLL /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v APILevel /t REG_EXPAND_SZ /d 1 /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v ConnectFunctions /t REG_EXPAND_SZ /d YYN /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v DriverODBCVer /t REG_EXPAND_SZ /d 03.00 /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v FileUsage /t REG_EXPAND_SZ /d 0 /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v SQLLevel /t REG_EXPAND_SZ /d 1 /f
+
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\ODBC Drivers" /v "DBMaker 5.4 bundle Driver" /t REG_EXPAND_SZ /d Installed /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBC.INI\ODBC Data Sources" /v UTF8DB /t REG_EXPAND_SZ /d "DBMaker 5.4 bundle Driver" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBC.INI\UTF8DB" /v Driver /t REG_EXPAND_SZ /d %DBMAKERPATH%\bundle\DMAPI54.DLL /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBC.INI\UTF8DB" /v Database /t REG_EXPAND_SZ /d UTF8DB /f
+
+)ELSE (
+echo "64bit odbc driver"
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v Driver /t REG_EXPAND_SZ /d %DBMAKERPATH%\bundle\DMAPI54.DLL /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v Setup /t REG_EXPAND_SZ /d %DBMAKERPATH%\bundle\DMSET.DLL /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v APILevel /t REG_EXPAND_SZ /d 1 /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v ConnectFunctions /t REG_EXPAND_SZ /d YYN /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v DriverODBCVer /t REG_EXPAND_SZ /d 03.00 /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v FileUsage /t REG_EXPAND_SZ /d 0 /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\DBMaker 5.4 bundle Driver" /v SQLLevel /t REG_EXPAND_SZ /d 1 /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" /v "DBMaker 5.4 bundle Driver" /t REG_EXPAND_SZ /d Installed /f
+REG ADD  "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBC.INI\ODBC Data Sources" /v UTF8DB /t REG_EXPAND_SZ /d "DBMaker 5.4 bundle Driver" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBC.INI\UTF8DB" /v Driver /t REG_EXPAND_SZ /d %DBMAKERPATH%\bundle\DMAPI54.DLL /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBC.INI\UTF8DB" /v Database /t REG_EXPAND_SZ /d UTF8DB /f
+
+)
---
+++ appveyor/test_dbmaker.cmd
@@ -0,0 +1,57 @@
+:dbmaker
+ECHO.
+ECHO ############################################################
+ECHO # DBMaker
+ECHO ############################################################
+set DBMAKERPATH=%cd%
+echo %DBMAKERPATH%
+cd %DBMAKERPATH%\\bundle
+echo [utf8db] >>dmconfig.ini
+echo db_lcode=10 >>dmconfig.ini
+echo db_clilcode=utf-8>>dmconfig.ini
+echo db_ptnum=2222>>dmconfig.ini
+echo db_svadr=127.0.0.1>>dmconfig.ini
+echo create db utf8db; > createutf8db.sql
+echo terminate db; >> createutf8db.sql
+echo q; >> createutf8db.sql
+echo dmsql32.exe createutf8db.sql >createutf8db.bat
+echo dmserver.exe UTF8DB \a >>createutf8db.bat
+echo exit; >> createutf8db.bat
+start createutf8db.bat
+cd ..
+IF %PYTHON_MAJOR_VERSION% EQU 2 (
+    SET TESTS_DIR=tests2
+) ELSE (
+    SET TESTS_DIR=tests3
+)
+SET CONN_STR=Driver=DBMaker 5.4 bundle Driver;Database=utf8db; uid=sysadm; pwd=;
+ECHO.
+ECHO *** Run tests using driver: "DBMaker 5.4 bundle Driver"
+copy %DBMAKERPATH%\\bundle\\dmconfig.ini dmconfig.ini
+echo %PYTHON_HOME%\python appveyor\test_connect.py "Driver=DBMaker 5.4 bundle Driver;Database=utf8db; uid=sysadm; pwd="
+"%PYTHON_HOME%\python" appveyor\test_connect.py "Driver=DBMaker 5.4 bundle Driver;Database=utf8db; uid=sysadm; pwd="
+IF ERRORLEVEL 1 (
+  ECHO *** ERROR: Could not connect using the connection string:
+  ECHO "%CONN_STR%"
+  SET OVERALL_RESULT=1
+  GOTO :end
+)
+echo %PYTHON_HOME%\python %TESTS_DIR%\dbmakertests.py -v "Driver=DBMaker 5.4 bundle Driver;Database=utf8db; uid=sysadm; pwd="
+"%PYTHON_HOME%\python" "%TESTS_DIR%\dbmakertests.py" -v "Driver=DBMaker 5.4 bundle Driver;Database=utf8db; uid=sysadm; pwd="
+IF ERRORLEVEL 1 SET OVERALL_RESULT=1
+
+echo delete dbmaker bundle
+cd %DBMAKERPATH%\\bundle
+echo connect to utf8db sysadm; > termutf8db.sql
+echo terminate db; >> termutf8db.sql
+echo q; >> termutf8db.sql
+echo terminate db
+dmsql32.exe termutf8db.sql
+echo del utf8db
+del UTF8DB.*
+cd ..
+echo rd bundle
+RD /S /Q "bundle"
+echo del dbmaker.zip
+del dbmaker.zip
+echo dbmaker over
