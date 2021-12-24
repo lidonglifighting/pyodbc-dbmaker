05163312e06b3e49c14dac14e5ceef3382db440e
lidonglifighting <42424903+lidonglifighting@users.noreply.github.com>
2021-12-24 14:31:31 +0800

    38

--- appveyor/build_script.cmd
+++ appveyor/build_script.cmd
@@ -60,6 +60,5 @@ ECHO *** Get version of the built pyodbc module:
 echo %path%
 copy %DBMAKERPATH%\bundle\dmapi54.dll %DBMAKERPATH%\build\lib.win32-3.8\
 
-#"%PYTHON_HOME%\python" -c "import pyodbc; print(pyodbc.version)"
 
 ECHO.
